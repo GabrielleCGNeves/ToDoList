d71f731d40702b8bc1a3639af24f9c54f45d9bc3
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-10-13 20:16:53 -0300

    :art: custom response entity

--- src/main/java/com/gabrielle/todolist/user/UserController.java
+++ src/main/java/com/gabrielle/todolist/user/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.var;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -19,17 +21,16 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel usermodel){
+    public ResponseEntity create(@RequestBody UserModel usermodel){
         
         var user = this.userRepository.findByUserName(usermodel.getUserName());
         
         if(user != null){
-            System.out.println("Usuário já existe.");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
         }
         
         var userCreated =  this.userRepository.save(usermodel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
         
     }
 }
